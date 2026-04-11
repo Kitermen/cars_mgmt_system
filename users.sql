034002cfbb4889eaa28f584d3364a0c3b96d55b5
siurban <oleczek.urban@gmail.com>
2026-04-11 16:29:46 +0200

    Update users.sql

--- users.sql
+++ users.sql
@@ -2,14 +2,14 @@
 -- Tabela użytkowników
 
 CREATE TABLE IF NOT EXISTS users (
-    user_id INTEGER PRIMARY KEY,
-    firstName TEXT NOT NULL,
-    lastName TEXT NOT NULL,
-    phone TEXT,
+    osoba_id INTEGER PRIMARY KEY,
+    imie TEXT NOT NULL,
+    nazwisko TEXT NOT NULL,
+    nr_telefon TEXT,
     email TEXT UNIQUE
 );
 
-INSERT INTO users (user_id, firstName, lastName, phone, email) VALUES
+INSERT INTO users (osoba_id, imie, nazwisko, nr_telefon, email) VALUES
 (1, 'Jan', 'Kowalski', '+48 600 100 200', 'jan.kowalski@example.com'),
 (2, 'Anna', 'Nowak', '+48 511 222 333', 'anna.nowak@example.com'),
 (3, 'Marek', 'Wiśniewski', '+48 789 456 123', 'marek.wisniewski@example.com'),
