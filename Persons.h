b333bbf1dccf2529bbe798e04f9f9415106457e8
yuriitsaltsalko <yurii.tsal-tsalko@student.pk.edu.pl>
2026-05-14 19:59:29 +0200

    update Persons.h
    update Persons.h for sqliteorm

--- Persons.h
+++ Persons.h
@@ -1,36 +1,25 @@
 #pragma once
-
 #include <iostream>
-#include <unistd.h>
 #include <string>
-#include <fstream>
-
 
 using namespace std;
 
+// Struktura mapująca tabelę 'users'
+struct User {
+    int user_id;
+    string firstName;
+    string lastName;
+    string phone;
+    string email;
+};
+
 class Osoba {
-
-    private:
-        string imie;
-        string nazwisko;
-        string nr_telefonu;
-        string email;
-
-    public:
-        Osoba(string imie, string nazwisko, string nr_telefonu, string email) :imie(imie), nazwisko(nazwisko), nr_telefonu(nr_telefonu), email(email) {}
-
-        void dodaj_osobe(){
-            cout << "Dodano nową osobę do bazy" << endl;
-            cout << imie << " " << nazwisko << " " << nr_telefonu << " " << email << endl;
-        }
-
-        void usun_osobe(){
-            cout << "Usunięto osobę do bazy" << endl;
-            cout << imie << " " << nazwisko << " " << nr_telefonu << " " << email << endl;
-
-        }
-
-
-
-    
+private:
+    string imie;
+    string nazwisko;
+public:
+    Osoba(string i, string n) : imie(i), nazwisko(n) {}
+    void dodaj_osobe() {
+        cout << "Dodano osobe: " << imie << " " << nazwisko << endl;
+    }
 };
